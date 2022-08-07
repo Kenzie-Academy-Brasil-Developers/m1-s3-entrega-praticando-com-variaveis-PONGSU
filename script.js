98a5fe7b76ce387fc1dfccbf148c5421dd28019d
GCM <gabrielc.maciel42@gmail.com>
2022-08-07 18:07:50 -0300

    Entrega S3 11 v1.0

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Gabriel"
+let sobrenome = "Maciel"
+let anoNascimento = Number(1994)
+let anoAtual = Number(2022)
+let idade = anoAtual - anoNascimento
+let cidade = "Nova Friburgo"
+
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`)
